2453877bc65853941275a96b6ec7801c67367d02
Brayan Jeshua <brayanjeshuavz@gmail.com>
2020-11-02 16:08:40 +0100

    Spread Operations

--- src/index.js
+++ src/index.js
@@ -13,3 +13,11 @@ let person = {
 let {name, country, age} = person;
 console.log (`${name} is from ${country} have ${age} years`)
 
+// Spread Operation
+let team1 = ['Jeshua', 'Oscar', 'Angelo']
+let team2 = ['Angie', 'Jessica', 'Maria']
+
+// Unir elementos en un Array
+let education = ['David', ...team1, ...team2]
+
+console.log(education)
