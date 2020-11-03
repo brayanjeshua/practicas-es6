02368f1c908f052fbe1b23844095ceb037679a9f
Brayan Jeshua <brayanjeshuavz@gmail.com>
2020-11-03 11:24:02 +0100

    Factorize callback

--- src/index.js
+++ src/index.js
@@ -2,35 +2,3 @@
 
 // API https://rickandmortyapi.com/api/
 
-// CallBacks
-
-// Suma de Dos Numeros
-function sum(num1, num2) {
-    return num1 + num2
-}
-
-function calc(num1,num2, callback) {
-    return callback(num1,num2)
-}
-
-console.log(calc(6, 3, sum))
-
-
-// Funcion para mostrar Fecha
-function date(callback) {
-    console.log("Empieza", new Date)
-    console.log("Espera 3 segundos...")
-    setTimeout(function () {
-        let date = new Date
-        callback(date)
-    }, 3000)
-}
-
-
-// Funcion para Imprimir
-function printDate(dateNow) {
-    console.log("Termina", dateNow)
-}
-
-// Llamar a la funcion date
-date(printDate)
