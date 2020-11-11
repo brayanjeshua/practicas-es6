6a6ba8c7de22375e79dbbb304d611946020a9d02
Brayan Jeshua <brayanjeshuavz@gmail.com>
2020-11-12 00:55:51 +0100

    Adding async/await code

--- src/async/index.js
+++ src/async/index.js
@@ -1,1 +1,31 @@
 
+const doSomethingAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+          ? setTimeout(() => resolve('Do something Async'), 3000)
+          : reject(new Error('Test Error'))
+    })
+}
+
+const doSomething = async () => {
+    const something = await doSomethingAsync()
+    console.log(something)
+}
+
+console.log('Before')
+doSomething()
+console.log('After')
+
+
+const anotherFunction = async () => {
+    try {
+        const something = await doSomethingAsync()
+        console.log(something)
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+console.log('Before another function')
+anotherFunction()
+console.log('After another function')
