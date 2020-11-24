cdb3f7ae85bac3da3fbf15ae006a8b81a561d893
Brayan Jeshua <brayanjeshuavz@gmail.com>
2020-11-24 23:27:10 +0100

    Adding new plugin

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,24 +1,39 @@
 function MediaPlayer(config) {
-this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function() {
-this.media.play();
-};
-
-MediaPlayer.prototype.pause = function() {
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+  
+    this._initPlugins();
+  }
+  
+  MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+      plugin.run(this);
+    });
+  };
+  
+  MediaPlayer.prototype.play = function() {
+    this.media.play();
+  };
+  
+  MediaPlayer.prototype.pause = function() {
     this.media.pause();
-};
-
-MediaPlayer.prototype.togglePlay = function() {
+  };
+  
+  MediaPlayer.prototype.togglePlay = function() {
     if (this.media.paused) {
-        this.play();
+      this.play();
     } else {
-        this.pause();
+      this.pause();
     }
-};
-
-
-
-
-export default MediaPlayer;
+  };
+  
+  MediaPlayer.prototype.mute = function() {
+    this.media.muted = true;
+  };
+  
+  MediaPlayer.prototype.unmute = function() {
+    this.media.muted = false;
+  };
+  
+  export default MediaPlayer;
+  
