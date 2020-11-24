aef8c28489e5a79acd0dedbceacfdc69c9d3ad8f
Brayan Jeshua <brayanjeshuavz@gmail.com>
2020-11-24 23:13:22 +0100

    Modules

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,9 +1,24 @@
-function  MediaPlayer(config) {
+function MediaPlayer(config) {
 this.media = config.el;
 }
 
 MediaPlayer.prototype.play = function() {
 this.media.play();
 };
+
+MediaPlayer.prototype.pause = function() {
+    this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function() {
+    if (this.media.paused) {
+        this.play();
+    } else {
+        this.pause();
+    }
+};
+
+
+
 
 export default MediaPlayer;
